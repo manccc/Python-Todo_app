307722d529e2ad4bae7147ccce3730cef3ea04d4
Mansi Dhavale <84283820+manccc@users.noreply.github.com>
2023-06-26 22:56:37 +0530

    settings.py

--- todo_app/settings.py
+++ todo_app/settings.py
@@ -23,7 +23,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-ei!4&uh3g5hkin-@o%f#)o03ed7mysv0mcbr3x!7mxhw81j(9s'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
 ALLOWED_HOSTS = ['*']
 
